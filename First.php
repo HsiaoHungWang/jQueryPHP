484045dd790cbc27d5236762146ea6c5caf37751
HsiaoHung Wang <wang@ispan.com.tw>
2023-06-15 16:21:25 +0800

    First Version

---
+++ First.php
@@ -0,0 +1,35 @@
+<?php
+ $connString = "mysql:host=localhost; port=3306; dbname=pubs; charset=utf8";
+ $user = "root";
+ $password = "root";
+ $accessOptions = array(
+      PDO::ATTR_EMULATE_PREPARES=>false,
+      PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION, 
+      PDO::ATTR_PERSISTENT => true
+ );
+ try {
+    //步驟一建立連線
+      $pdo = new PDO($connString, $user, $password, $accessOptions);
+     // echo "連結資料庫成功!!!";
+
+//步驟二下命令(SQL語法)
+$sql = "SELECT * FROM `authors` WHERE state='CA'";
+$stmt = $pdo->prepare($sql);
+$stmt->execute(); 
+$rows = $stmt->fetchAll(); //讀取所有資料
+
+//步驟三將讀出的結果轉成json格式回傳給瀏覽器
+echo json_encode($rows);
+
+
+
+ } catch(Exception $ex){
+      echo "存取資料庫時發生錯誤，訊息:" . $ex->getMessage() . "<br>";
+      echo "苦主:" . $ex->getFile() . "<br>";
+      echo "行號:" . $ex->getLine() . "<br>";
+      echo "Code:" . $ex->getCode() . "<br>";
+      echo "堆疊:" . $ex->getTraceAsString() . "<br>";
+ }
+
+
+?>
