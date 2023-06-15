67c33c1c0ec1d9767812a6ffea999500a97c87fd
HsiaoHung Wang <wang@ispan.com.tw>
2023-06-15 16:28:58 +0800

    UTF8設定

--- First.php
+++ First.php
@@ -5,7 +5,8 @@
  $accessOptions = array(
       PDO::ATTR_EMULATE_PREPARES=>false,
       PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION, 
-      PDO::ATTR_PERSISTENT => true
+      PDO::ATTR_PERSISTENT => true,
+      PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"
  );
  try {
     //步驟一建立連線
